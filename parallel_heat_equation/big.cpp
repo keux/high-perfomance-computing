d8c29c6faec18fe01c4b4dc9645f71206e8bce84
keux <45452453+keux@users.noreply.github.com>
2020-02-18 23:10:46 +0300

    Add files via upload

---
+++ parallel_heat_equation/big.cpp
@@ -0,0 +1,130 @@
+#include <mpi.h>
+#include <time.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <math.h>
+
+void step(const std::vector<double>& Ui, std::vector<double>& Ui1, int size, double rl, double rr, double dx,
+        double dt, int r, int pc)
+{
+    double C = dt / (dx*dx);
+    // double del_k = 8 * std::pow(10, 4); // чтобы удовлетворяло условиям
+    // C /= del_k;
+    C *= (0.01 / C);
+
+    for (int i = 0; i < size; ++i)
+    {
+        if (i == 0)
+        {
+            if (r == 0)
+                Ui1[i] = 0;
+            else
+                Ui1[i] = Ui[i] + C * (rl - 2 * Ui[i] + Ui[i + 1]);
+        } else if (i == size - 1) {
+            if (r == pc - 1)
+                Ui1[i] = 0;
+            else
+                Ui1[i] = Ui[i] + C * (Ui[i - 1] - 2 * Ui[i] + rr);
+
+        } else {
+            Ui1[i] = Ui[i] + C * (Ui[i - 1] - 2 * Ui[i] + Ui[i + 1]);
+        }
+    }
+}
+
+void printTimeRank(int threadsNum, std::vector<double>& Ui,  int size,  int rank)
+{
+    char name[10];
+    sprintf(name, "Prog2v2_%i_Rank%i", threadsNum, rank);
+    std::ofstream myfile;
+    myfile.open(name, std::fstream::in | std::fstream::out | std::fstream::app);
+    for (int i = 0; i < size; i += 5)
+    {
+        if(i != size - 1)
+            myfile << Ui[i] << "\t";
+        else
+            myfile << Ui[i] << "\t\n";
+    }
+    myfile << "\n";
+    myfile.close();
+}
+void solveHeat(std::vector<double>& U, double a, double b, double Nx, int rank, int sizeProc, double dx, double dt, int NTimeSteps)
+{
+
+    int Nr = Nx / sizeProc;
+    int start = Nr * rank;
+    if (rank == sizeProc - 1)
+        Nr = Nx - start;
+    int end = start + Nr;
+    int size = Nr;
+
+    std::vector<double> Ui(size), Ui1(size);
+    for (int i = start; i < end; ++i)
+    {
+        Ui[i - start] = U[start*0 + i];
+        Ui1[i - start] = U[start*0 + i];
+    }
+
+    // auto upTo = std::pow(NTimeSteps, sizeProc) * 10;
+    // std::cout << "NTimeSteps = " << upTo << "\n";
+    auto upTo = NTimeSteps * sizeProc * 100;
+    for (int s = 0; s < upTo; ++s)
+    {
+        double sl = Ui[0], sr = Ui[size - 1];
+        double rl = Ui[0], rr = Ui[size - 1];
+
+        MPI_Status status;
+
+        if (rank < sizeProc - 1)
+            MPI_Send((&sr), 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD);
+        if (rank > 0)
+            MPI_Recv((&rl), 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &status);
+
+        if (rank > 0)
+            MPI_Send((&sl), 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
+        if (rank < sizeProc - 1)
+            MPI_Recv((&rr), 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &status);
+
+
+        step(Ui, Ui1, size, rl, rr, dx, dt, rank, sizeProc);
+        for (int i = start; i < end; ++i)
+        {
+            U[start*0 + i] = Ui1[i - start];
+            // std::cout << Ui1[i] << "\n";
+        }
+        Ui.swap(Ui1);
+        ;
+    }
+    printTimeRank(sizeProc, Ui1, size, rank);
+}
+
+int main(int argc, char** argv)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    double a = 0, b = 1, t0 = 0, t1 = 0.001;
+    int Nx = 50000;
+    double dx = (b-a) / Nx, dt = 0.0002;
+    int NTimeSteps = (t1-t0) / dt;
+
+    MPI_Init(&argc, &argv);
+    int sizeProc = 0;
+    int rank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
+
+    std::vector<double> U(Nx, 1);
+
+    solveHeat(U, a, b, Nx, rank, sizeProc, dx, dt, NTimeSteps);
+
+    MPI_Finalize();
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = stop - start;
+
+    std::cout << "\nElapsed time for " << sizeProc << " threads = " << duration.count() << std::endl;
+
+    return 0;
+}
